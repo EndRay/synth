7c404b9053a96d11a081cab01cba78ad120af877
Maksym <maksym.tur@student.uj.edu.pl>
2022-05-02 11:56:57 +0200

    Added measure divisions to Sequence.

--- src/main/java/sequencer/Sequence.java
+++ src/main/java/sequencer/Sequence.java
@@ -3,9 +3,15 @@ package sequencer;
 import java.util.*;
 
 public class Sequence implements Iterable<Step>{
+    private MeasureDivision measureDivision;
     private final List<Step> steps = new ArrayList<>();
 
     public Sequence(Step... steps) {
+        this(MeasureDivision.SIXTEENTH, steps);
+    }
+
+    public Sequence(MeasureDivision measureDivision, Step... steps) {
+        this.measureDivision = measureDivision;
         Collections.addAll(this.steps, steps);
     }
 
@@ -24,5 +30,13 @@ public class Sequence implements Iterable<Step>{
     @Override
     public ListIterator<Step> iterator() {
         return getSteps().listIterator();
+    }
+
+    public MeasureDivision getMeasureDivision() {
+        return measureDivision;
+    }
+
+    public void setMeasureDivision(MeasureDivision measureDivision) {
+        this.measureDivision = measureDivision;
     }
 }
