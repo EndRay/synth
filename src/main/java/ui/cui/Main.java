ea62db0a77dbb94b3a681c4bf89fe43777ac3932
EndRay <a.i.kovrigin@gmail.com>
2022-05-26 02:52:09 +0200

    demo synths moved and removed

--- src/main/java/ui/cui/Main.java
+++ src/main/java/ui/cui/Main.java
@@ -12,11 +12,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
+import static database.Database.loadSynthsFromFolder;
 import static synthesizer.sources.SignalSource.sampleRate;
 
 public class Main {
 
     public static void main(String[] args) {
+        loadSynthsFromFolder(new File("wickedChildSynths"));
         ConsoleHandler handler = new ConsoleHandler();
         if(args.length > 0){
             try{
